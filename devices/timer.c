bbc62da07916128ddd047dff34dbb5e0b2b0ea7d
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-02 00:14:01 +0900

    Update timer.c

--- devices/timer.c
+++ devices/timer.c
@@ -154,7 +154,7 @@ timer_print_stats (void) {
 
 
 /* Alarm function. */
-static void timer_alarm(){
+static void timer_alarm(void){
 	//iterate through asleep_list, wake up any threads that are past "wakeuptime".
 	struct list_elem* i;
 	i = list_begin(&asleep_list);
