6a0efb4cf278becbfe095fca599ae28c28d04923
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-02 14:06:06 +0900

    Update synch.c

--- threads/synch.c
+++ threads/synch.c
@@ -78,12 +78,7 @@ sema_down (struct semaphore *sema) {
 
 	old_level = intr_disable ();
 	while (sema->value == 0) {
-		/* NEWCODE */
-		//same as priority scheduling. use insert_ordered
-		list_insert_ordered(&sema->waiters,  &thread_current()->elem, compare_pri, NULL);
-		/* ENDOFNEWCODE */
-		
-		//list_push_back (&sema->waiters, &thread_current ()->elem);
+		list_push_back (&sema->waiters, &thread_current ()->elem);
 		thread_block ();
 	}
 	sema->value--;
