87e459058eea0f5bcfa95be88b2c984762cba884
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-02 13:17:55 +0900

    Update synch.c

--- threads/synch.c
+++ threads/synch.c
@@ -131,6 +131,7 @@ sema_up (struct semaphore *sema) {
 		thread_unblock (list_entry (list_pop_front (&sema->waiters),
 					struct thread, elem));
 	sema->value++;
+	thread_yield();
 	intr_set_level (old_level);
 	
 	/* NEWCODE */
