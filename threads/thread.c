ddf2979ac031f8f47efc5d65c7abe2d56144ecfc
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-02 12:22:34 +0900

    Update thread.c

--- threads/thread.c
+++ threads/thread.c
@@ -259,7 +259,7 @@ thread_unblock (struct thread *t) {
 	old_level = intr_disable ();
 	ASSERT (t->status == THREAD_BLOCKED);
 	//list_push_back (&ready_list, &t->elem);
-	list_insert_ordered (&ready_list, &curr->elem, compare_pri, NULL);
+	list_insert_ordered (&ready_list, &t->elem, compare_pri, NULL);
 	t->status = THREAD_READY;
 	intr_set_level (old_level);
 }
