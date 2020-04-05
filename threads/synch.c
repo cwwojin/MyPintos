14b7122b7d549084fe1d0597da93fc5d2484f515
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-05 16:06:51 +0900

    Update synch.c

--- threads/synch.c
+++ threads/synch.c
@@ -282,7 +282,7 @@ static void remove_from_donations(struct lock* lock){
 	struct list_elem* e;
 	struct list_elem* delete = NULL;
 	if(list_empty(&(current->donation_list))) return;
-	
+	/*
 	for (e = list_begin (&(current->donation_list)); e != list_tail(&(current->donation_list)); e = list_next(e)) {
  		struct thread *ethread = list_entry(e, struct thread, elem);
 		if(lock == ethread->gate){
@@ -290,6 +290,9 @@ static void remove_from_donations(struct lock* lock){
 			break;
 		}
 	}
+	*/
+	e = list_begin(&(current->donation_list));
+	delete = e;
 	if(delete != NULL) list_remove(delete);
 	
 	
