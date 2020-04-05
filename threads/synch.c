19305bd17bd4ff067555235d7c97b750174b2be9
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-05 20:18:53 +0900

    Update synch.c

--- threads/synch.c
+++ threads/synch.c
@@ -211,7 +211,8 @@ static void donate_priority(void){
 	if(waitinglock == NULL) return;
 	if(t->priority > waitinglock->holder->priority){
 		printf("am going to insert.\n");
-		list_insert_ordered (&(waitinglock->holder->donation_list), &t->elem, compare_pri, NULL);
+		//list_insert_ordered (&(waitinglock->holder->donation_list), &t->elem, compare_pri, NULL);
+		list_push_back(&waitinglock->holder->donation_list, &t->elem);
 		printf("inserted successfully.\n");
 	}
 	
