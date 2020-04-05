8d2f9f2d7755ec07a6a4d8ebc62245a3d8b54b72
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-05 15:53:34 +0900

    Update synch.c

--- threads/synch.c
+++ threads/synch.c
@@ -280,7 +280,7 @@ static void remove_from_donations(struct lock* lock){
 	struct thread* current = thread_current();
 	//iterate through current thread's donation list
 	struct list_elem* e;
-	struct list_elem* delete;
+	struct list_elem* delete = NULL;
 	if(list_empty(&(current->donation_list))) return;
 	
 	for (e = list_begin (&(current->donation_list)); e != list_end (&(current->donation_list)); e = list_next(e)) {
