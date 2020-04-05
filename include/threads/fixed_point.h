719957e2297eed0e35125d326204ab86594372b1
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-05 22:04:55 +0900

    Update fixed_point.h

--- include/threads/fixed_point.h
+++ include/threads/fixed_point.h
@@ -1,18 +1,18 @@
-#define F (1<< 14)//fixed point 1
-#define INT_MAX((1<< 31) -1)
-#define INT_MIN(-(1<< 31))
+#define F (1<< 14)
+#define INT_MAX ((1<< 31) -1)
+#define INT_MIN (-(1<< 31))
 // x and y denote fixed_pointnumbers in 17.14 format
 // n is an integer
 
 
-intint_to_fp(intn);/* integer를fixed point로전환*/
-intfp_to_int_round(intx);/* FP를int로전환(반올림) */
-intfp_to_int(intx);/* FP를int로전환(버림) */
-intadd_fp(intx,inty);/* FP의덧셈*/
-intadd_mixed(intx, intn);/* FP와int의덧셈*/
-intsub_fp(intx, inty);/* FP의뺄셈(x-y) */
-intsub_mixed(intx, intn);/* FP와int의뺄셈(x-n) */
-intmult_fp(intx, inty);/* FP의곱셈*/
-intmult_mixed(intx,inty);/* FP와int의곱셈*/
-intdiv_fp(intx, inty);/* FP의나눗셈(x/y) */
-intdiv_mixed(intx, intn);/* FP와int나눗셈(x/n) */
+int int_to_fp(int n); /* integer를fixed point로전환*/
+int fp_to_int_round(int x); /* FP를int로전환(반올림) */
+int fp_to_int(int x); /* FP를int로전환(버림) */
+int add_fp(int x, int y); /* FP의덧셈*/
+int add_mixed(int x, int n);  /* FP와int의덧셈*/
+int sub_fp(int x, int y); /*FP의뺄셈(x-y) */
+int sub_mixed(int x, int n);  /* FP와int의뺄셈(x-n) */
+int mult_fp(int x, int y);  /* FP의곱셈*/
+int mult_mixed(int x,int y);  /* FP와int의곱셈*/
+int div_fp(int x, int y); /* FP의나눗셈(x/y) */
+int div_mixed(int x, int n);  /* FP와int나눗셈(x/n) */
