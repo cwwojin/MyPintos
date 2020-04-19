b02fad2dbda2f57c448d33436f973a13d3ae1a0a
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-19 14:53:19 +0900

    Update thread.h

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -107,6 +107,7 @@ struct thread {
 #ifdef USERPROG
 	/* Owned by userprog/process.c. */
 	uint64_t *pml4;                     /* Page map level 4 */
+	int status;		/* Exit Status */
 #endif
 #ifdef VM
 	/* Table for whole virtual memory owned by thread. */
