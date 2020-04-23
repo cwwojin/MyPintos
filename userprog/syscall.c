8d2eaa41d4dc1914057d21ab6a7ae6ce19642a53
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-23 11:34:55 +0900

    Update syscall.c

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -11,6 +11,7 @@
 #include "threads/synch.h"
 #include "filesys/filesys.h"
 #include "filesys/file.h"
+#include "userprog/process.h"
 
 void syscall_entry (void);
 void syscall_handler (struct intr_frame *);
