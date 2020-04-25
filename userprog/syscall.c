fad90be6f717d14158775673476b421c540e1702
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-25 19:23:30 +0900

    Update syscall.c

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -8,7 +8,7 @@
 #include "threads/flags.h"
 #include "intrinsic.h"
 
-#include <syscall.h>
+#include "include/lib/user/syscall.h"
 #include <string.h>
 #include "threads/synch.h"
 #include "threads/init.h"
