e5fe97e159b50324f3acceb4c2e50490862a6b9a
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-25 22:07:48 +0900

    Update file.c

--- filesys/file.c
+++ filesys/file.c
@@ -10,8 +10,6 @@ struct file {
 	struct inode *inode;        /* File's inode. */
 	off_t pos;                  /* Current position. */
 	bool deny_write;            /* Has file_deny_write() been called? */
-	
-	struct list_elem elem;		/* List elem. */
 };
 
 /* Opens a file for the given INODE, of which it takes ownership,
