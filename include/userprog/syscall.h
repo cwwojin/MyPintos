25db63f1644977d50401eabebbb03d0f36b0bb20
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-25 18:14:17 +0900

    Update syscall.h

--- include/userprog/syscall.h
+++ include/userprog/syscall.h
@@ -5,7 +5,5 @@ void syscall_init (void);
 
 void exit(int status);
 void check_address(void* addr);
-bool create(const char *file, unsigned initial_size);
-bool remove(const char* file);
 
 #endif /* userprog/syscall.h */
