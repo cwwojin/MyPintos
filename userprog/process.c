261508aa4a23fd23e3a14587d3429af341581be2
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-28 03:14:25 +0900

    Update process.c

--- userprog/process.c
+++ userprog/process.c
@@ -185,6 +185,8 @@ process_fork (const char *name, struct intr_frame *if_ UNUSED) {
 	struct pcb* child_pcb = get_child_process(child);
 	if(child_pcb->exit_status == -1){
 		printf("at parent : %d, child : %d, fork fail due to resource duplication fail.\n", current->tid, child_pcb->tid);
+		int wait = wait(child);
+		printf("child exited with status %d, and pcb is freed.", wait);
 		return TID_ERROR;
 	}
 	
