a5a960ee306e452a98d18bbbdabb7842072cc727
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-04-29 19:10:52 +0900

    Update thread.c

--- threads/thread.c
+++ threads/thread.c
@@ -280,6 +280,7 @@ thread_create (const char *name, int priority,
    primitives in synch.h. */
 void
 thread_block (void) {
+	ASSERT (thread_current() != idle_thread);
 	ASSERT (!intr_context ());
 	ASSERT (intr_get_level () == INTR_OFF);
 	
