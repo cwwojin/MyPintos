9b147cde8245337a2dab2b55423d7420e2489f48
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-05-20 14:50:26 +0900

    Update vm.h

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -82,6 +82,7 @@ struct page_operations {
  * We don't want to force you to obey any specific design for this struct.
  * All designs up to you for this. */
 struct supplemental_page_table {
+	struct hash hash;
 };
 
 #include "threads/thread.h"
