82e2e7e8e3c6faea1d130b15c02349c7111526d0
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-05-25 15:53:52 +0900

    Update process.c

--- userprog/process.c
+++ userprog/process.c
@@ -934,7 +934,7 @@ setup_stack (struct intr_frame *if_) {
 	 * TODO: If success, set the rsp accordingly.
 	 * TODO: You should mark the page is stack. */
 	/* TODO: Your code goes here */
-	success = vm_alloc_page(VM_MARKER_0, stack_bottom, true);
+	success = vm_alloc_page(VM_MARKER_0 + VM_ANON, stack_bottom, true);
 	if(success){
 		success = vm_claim_page(stack_bottom);
 	}
