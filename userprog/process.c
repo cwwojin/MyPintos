a602b6f86ef01eeb798aaa5c00560f86e358da01
cwwojin <55651036+cwwojin@users.noreply.github.com>
2020-05-25 21:05:00 +0900

    Update process.c

--- userprog/process.c
+++ userprog/process.c
@@ -916,9 +916,10 @@ load_segment (struct file *file, off_t ofs, uint8_t *upage,
 		aux = AUX;
 		printf("aux : %X. Going to READ : %d & ZERO : %d bytes to page at Uaddr : %X..\n",aux, page_read_bytes,page_zero_bytes,upage);
 		if (!vm_alloc_page_with_initializer (VM_ANON, upage,
-					writable, lazy_load_segment, aux))
+					writable, lazy_load_segment, aux)){
+			printf("Alloc failed..\n");
 			return false;
-
+		}
 		/* Advance. */
 		read_bytes -= page_read_bytes;
 		zero_bytes -= page_zero_bytes;
