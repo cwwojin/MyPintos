0b939df19610b8f9f0f23bd14a83811e25455e06
Woojin Choi <55651036+cwwojin@users.noreply.github.com>
2020-06-05 01:13:14 +0900

    Update anon.h

--- include/vm/anon.h
+++ include/vm/anon.h
@@ -6,6 +6,10 @@ enum vm_type;
 
 #include <inttypes.h>
 #include <stdint.h>
+#include "devices/disk.h"
+#include "threads/vaddr.h"
+
+#define SECTORS_PER_PAGE  (PGSIZE / DISK_SECTOR_SIZE)
 
 struct anon_page {
   vm_initializer* init;
@@ -13,6 +17,13 @@ struct anon_page {
   enum vm_type type;
   uint32_t slotNo;
 };
+
+struct swap_slot{  //This is an entry in the swap table.
+  struct list_elem elem;
+  bool free;
+  uint32_t slotNo;  //Slot number : This corresponds to the swap disk's sector #(slotNo * 8). 
+};
+
 
 void vm_anon_init (void);
 bool anon_initializer (struct page *page, enum vm_type type, void *kva);
