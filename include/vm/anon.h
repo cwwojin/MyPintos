070c7cfb997018ecb5db43a121f287e6607fa3c5
Woojin Choi <55651036+cwwojin@users.noreply.github.com>
2020-06-04 23:00:23 +0900

    Update anon.h

--- include/vm/anon.h
+++ include/vm/anon.h
@@ -4,9 +4,14 @@
 struct page;
 enum vm_type;
 
+#include <inttypes.h>
+#include <stdint.h>
+
 struct anon_page {
   vm_initializer* init;
   struct lazy_aux* aux;
+  enum vm_type type;
+  uint32_t slotNo;
 };
 
 void vm_anon_init (void);
