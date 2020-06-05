823c8ce515a4d006ace0624c81f59f8842a2e3ae
Woojin Choi <55651036+cwwojin@users.noreply.github.com>
2020-06-05 21:58:34 +0900

    Update vm.c

--- vm/vm.c
+++ vm/vm.c
@@ -336,7 +336,7 @@ supplemental_page_table_copy (struct supplemental_page_table *dst UNUSED,
 			/* COPY-ON-WRITE : Instead of claiming page here, just add the pml4 mapping & set write-protected!! */
 			//printf("Copying page : 0x%X <-> KVA : 0x%X mapping..\n", p->va, p->frame->kva);
 			//pml4_set_page(thread_current()->pml4, newp->va, p->frame->kva, false);
-			if(p->writable){	//stack page!!
+			if(p->uninit.type == VM_MARKER_0 + VM_ANON){	//stack page!!
 				printf("Copying page : 0x%X..\n", p->va);
 				if(!vm_do_claim_page(newp)){
 					printf("SPT_COPY : failed to claim page.\n");
