14e4453f18c1ee8d9d5c94a8d02087c60d4594b9
Woojin Choi <55651036+cwwojin@users.noreply.github.com>
2020-06-05 14:31:36 +0900

    Update file.c

--- vm/file.c
+++ vm/file.c
@@ -36,24 +36,53 @@ file_map_initializer (struct page *page, enum vm_type type, void *kva) {
 	struct lazy_aux* AUX = page->uninit.aux;
 	file_page->file = AUX != NULL ? AUX->executable : NULL;
 	file_page->next_page = AUX->next_page;
+	file_page->swapped_out = false;
 }
 
 /* Swap in the page by read contents from the file. */
 static bool
 file_map_swap_in (struct page *page, void *kva) {
-	struct file_page *file_page UNUSED = &page->file;
+	struct file_page *file_page = &page->file;
+	if(!file_page->swapped_out){				//This page wasn't swapped out before. Finish here.
+		return true;
+	}
+	uint8_t *kpage = page->frame->kva;
+	struct lazy_aux* auz = page->uninit.aux;
+	size_t page_read_bytes = aux->page_read_bytes;
+	size_t page_zero_bytes = aux->page_zero_bytes;
+	off_t ofs = aux->offset;
+	struct file* file = file_page->file;
+	if (kpage == NULL)
+		return false;
+	file_seek(file, ofs);
+	int read = file_read (file, kpage, page_read_bytes);
+	if (read != (int) page_read_bytes) {
+		printf("load failed @ page 0x%X, read %d bytes instead of %d..\n", page->va, read, page_read_bytes);
+		return false;
+	}
+	memset (kpage + page_read_bytes, 0, page_zero_bytes);
+	file_page->read_bytes = page_read_bytes;
+	file_page->swapped_out = false;				//This page is now not swapped-out.
+	return true;
 }
 
 /* Swap out the page by writeback contents to the file. */
 static bool
 file_map_swap_out (struct page *page) {
-	struct file_page *file_page UNUSED = &page->file;
+	struct file_page *file_page = &page->file;
+	if(pml4_is_dirty(thread_current()->pml4, page->va)){	//Write back contents to file, if DIRTY.
+		off_t written = file_write_at(file_page->file, page->va, file_page->read_bytes, file_page->aux->offset);
+		if(written != file_page->read_bytes){
+			printf("SWAP-OUT page 0x%X -> read_bytes : %d, offset : %d, WRITTEN : %d bytes.\n", page->va, file_page->read_bytes, file_page->aux->offset, written);
+		}
+	}
+	file_page->swapped_out = true;				//mark TRUE for later SWAP-IN's.
 }
 
 /* Destory the file mapped page. PAGE will be freed by the caller. */
 static void
 file_map_destroy (struct page *page) {
-	struct file_page *file_page UNUSED = &page->file;
+	struct file_page *file_page = &page->file;
 	if(pml4_is_dirty(thread_current()->pml4, page->va)){	//Write back contents to file, if DIRTY.
 		off_t written = file_write_at(file_page->file, page->va, file_page->read_bytes, file_page->aux->offset);
 		if(written != file_page->read_bytes){
