37252e11a6932809bc6eddc1d1bf6f8532270cfa
Woojin Choi <55651036+cwwojin@users.noreply.github.com>
2020-06-05 13:24:22 +0900

    Update anon.c

--- vm/anon.c
+++ vm/anon.c
@@ -21,6 +21,7 @@ static const struct page_operations anon_ops = {
 
 static struct list swap_list;	//swap table.
 static struct lock swap_lock;	//swap lock -> use this when modifying swap slots.
+static int nSlots;		//MAX #. of slots, index goes up to 0 ~ (nSlots - 1).
 static void swap_init(void);
 
 /* Initialize the data for anonymous pages */
@@ -33,8 +34,8 @@ vm_anon_init (void) {
 	swap_init();
 }
 
-static void swap_init(void){	//initialize all swap table slots.
-	int nSlots = ((int) disk_size(swap_disk)) / SECTORS_PER_PAGE;
+static void swap_init(void){
+	nSlots = ((int) disk_size(swap_disk)) / SECTORS_PER_PAGE;
 	printf("nSlots : %d\n",nSlots);
 }
 
@@ -48,7 +49,33 @@ anon_initializer (struct page *page, enum vm_type type, void *kva) {
 	anon_page->init = page->uninit.init;
 	anon_page->aux = page->uninit.aux;
 	anon_page->type = page->uninit.type;
-	anon_page->slotNo = NULL;
+	anon_page->slot = NULL;
+}
+
+/* Get an available swap slot. if None exist and total slots < nSlots, then allocate a NEW one. */
+static struct swap_slot* get_available_slot(void){
+	struct list_elem* e;
+	struct swap_slot* target = NULL;
+	for(e = list_begin(&swap_list); e != list_end(&swap_list); e = list_next(e)){
+		slot = list_entry(e, struct swap_slot, elem);
+		if(slot->free){	//This slot is FREE. choose this.
+			slot->free = false;
+			target = slot;
+			break;
+		}
+	}
+	if(target == NULL){	//None of the slots were available, allocate a NEW one.
+		int size = (int) list_size(&swap_list);
+		if(size > nSlots){
+			printf("No more swap-slots!!!\n");
+			return NULL;
+		}
+		target = malloc(sizeof(struct swap_slot));
+		target->free = false;
+		target->slotNo = size;
+	}
+	ASSERT(target != NULL);
+	return target;
 }
 
 /* Swap in the page by read contents from the swap disk. */
