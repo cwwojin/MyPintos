1a915a5ed8d42cf22a55f30f9905e0d8c8aa45b9
Woojin Choi <55651036+cwwojin@users.noreply.github.com>
2020-06-05 12:37:27 +0900

    Update vm.c

--- vm/vm.c
+++ vm/vm.c
@@ -120,7 +120,6 @@ spt_remove_page (struct supplemental_page_table *spt, struct page *page) {
 /* NEWCODE : sweep through the frame table, increment CNT if not used. */
 void vm_sweep_frame_table(void){
 	struct list_elem* e;
-	lock_acquire(&frame_lock);
 	for(e = list_begin(&frame_list); e != list_end(&frame_list); e = list_next(e)){
 		struct frame* frame = list_entry(e, struct frame, elem);
 		struct thread* owner = frame->owner;
@@ -134,7 +133,6 @@ void vm_sweep_frame_table(void){
 			pml4_set_accessed(owner->pml4, frame->kva, false);
 		}
 	}
-	lock_release(&frame_lock);
 }
 
 /* Get the struct frame, that will be evicted. */
