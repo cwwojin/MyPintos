747475b6e732c4c40b436fcfb16c2d43ffea3f0a
Woojin Choi <55651036+cwwojin@users.noreply.github.com>
2020-06-05 15:27:25 +0900

    Update vm.c

--- vm/vm.c
+++ vm/vm.c
@@ -257,7 +257,7 @@ vm_do_claim_page (struct page *page) {
 	struct frame *frame = vm_get_frame ();
 	if(frame == NULL){
 		printf("page 0x%X failed to get a frame!\n",page->va);
-		switch(vm_get_type(page)){
+		switch(page_get_type(page)){
 			case VM_ANON:
 				printf("ANON, slotNo : %d\n", page->anon.slot == NULL ? page->anon.slot->slotNo : -1);
 				break;
