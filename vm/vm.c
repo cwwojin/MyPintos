8bbcb1a5dbbc9f09133e04405445285f5239df29
Woojin Choi <55651036+cwwojin@users.noreply.github.com>
2020-06-05 21:39:13 +0900

    Update vm.c

--- vm/vm.c
+++ vm/vm.c
@@ -337,7 +337,7 @@ supplemental_page_table_copy (struct supplemental_page_table *dst UNUSED,
 			//printf("Copying page : 0x%X <-> KVA : 0x%X mapping..\n", p->va, p->frame->kva);
 			//pml4_set_page(thread_current()->pml4, newp->va, p->frame->kva, false);
 			
-			if(!p->writable){	//stack page!!
+			if(p->writable){	//stack page!!
 				printf("Copying WRITE-PROTECTED page : 0x%X..\n", p->va);
 				if(!vm_do_claim_page(newp)){
 					printf("SPT_COPY : failed to claim page.\n");
