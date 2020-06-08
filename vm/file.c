f474ffdb62a54ae21831cdadd8538da11cb4ea41
Woojin Choi <55651036+cwwojin@users.noreply.github.com>
2020-06-08 13:55:58 +0900

    Update file.c

--- vm/file.c
+++ vm/file.c
@@ -89,7 +89,9 @@ file_map_destroy (struct page *page) {
 	struct file_page *file_page = &page->file;
 	if(pml4_is_dirty(thread_current()->pml4, page->va)){	//Write back contents to file, if DIRTY.
 		off_t written = file_write_at(file_page->file, page->va, file_page->read_bytes, file_page->aux->offset);
-		ASSERT(written != file_page->read_bytes);
+		if(written != file_page->read_bytes){
+			printf("DESTROY page 0x%X -> read_bytes : %d, offset : %d, WRITTEN : %d bytes.\n", page->va, file_page->read_bytes, file_page->aux->offset, written);
+		}
 		pml4_set_dirty(thread_current()->pml4, page->va, false);
 	}
 	if(file_page->aux != NULL){	//free the LAZY_AUX.
