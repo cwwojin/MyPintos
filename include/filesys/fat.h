4d6bc27fb33f0b6b51594b717eaf4af28b3631f8
Woojin Choi <55651036+cwwojin@users.noreply.github.com>
2020-07-02 22:35:30 +0900

    Update fat.h

--- include/filesys/fat.h
+++ include/filesys/fat.h
@@ -34,5 +34,6 @@ void fat_remove_chain (
 cluster_t fat_get (cluster_t clst);
 void fat_put (cluster_t clst, cluster_t val);
 disk_sector_t cluster_to_sector (cluster_t clst);
+disk_sector_t fat_traverse(cluster_t start, unsigned int n);
 
 #endif /* filesys/fat.h */
