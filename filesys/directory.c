3ec802839add561bfa9d05d2d1c84c7f6a1a8fa9
Woojin Choi <55651036+cwwojin@users.noreply.github.com>
2020-07-03 12:13:22 +0900

    Update directory.c

--- filesys/directory.c
+++ filesys/directory.c
@@ -26,7 +26,11 @@ struct dir_entry {
  * given SECTOR.  Returns true if successful, false on failure. */
 bool
 dir_create (disk_sector_t sector, size_t entry_cnt) {
+#ifdef EFILESYS
+	return inode_create (sector_to_cluster(sector), entry_cnt * sizeof (struct dir_entry));
+#else
 	return inode_create (sector, entry_cnt * sizeof (struct dir_entry));
+#endif
 }
 
 /* Opens and returns the directory for the given INODE, of which
