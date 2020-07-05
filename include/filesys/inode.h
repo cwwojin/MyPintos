440bdf5042b66bcf8e4749a1c508d5a8d6dd0b5a
Woojin Choi <55651036+cwwojin@users.noreply.github.com>
2020-07-05 20:38:54 +0900

    Update inode.h

--- include/filesys/inode.h
+++ include/filesys/inode.h
@@ -8,7 +8,11 @@
 struct bitmap;
 
 void inode_init (void);
+#ifdef EFILESYS
+bool inode_create (cluster_t, off_t, bool);
+#else
 bool inode_create (disk_sector_t, off_t);
+#endif
 struct inode *inode_open (disk_sector_t);
 struct inode *inode_reopen (struct inode *);
 disk_sector_t inode_get_inumber (const struct inode *);
